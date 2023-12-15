b26c5a79421156934c19e0253a2ec1915e994d42
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2023-12-15 20:26:05 +0100

    Video backgrounds

--- static/js/index.js
+++ static/js/index.js
@@ -3,6 +3,10 @@ function tryToLoad() {
         document.querySelector("#loading").classList.add("d-none");
         document.querySelector("#offline").classList.add("d-none");
         document.querySelector("#main").classList.remove("d-none");
+
+        var videoDiv = document.createElement('div');
+        videoDiv.innerHTML = '<video id="background-video" autoplay loop muted src="https://cdn.jsdelivr.net/gh/bartekl1/MeteoHomepage@main/videos/2.mp4"></video>';
+        document.querySelector('body').appendChild(videoDiv)
     } else {
         document.querySelector("#loading").classList.add("d-none");
         document.querySelector("#offline").classList.remove("d-none");
