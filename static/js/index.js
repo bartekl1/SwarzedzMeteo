9c678140324cccaf4be677656b9345ca425eaf42
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2023-12-15 21:23:27 +0100

    Background videos from jsDelivr CDN

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,29 @@
+function getRandomVideo() {
+    const min = 1;
+    const max = 17;
+    const ignore = [11];
+    const user = "bartekl1";
+    const repo = "SwarzedzMeteoBackgrounds";
+    const resolution = 720;
+    
+    rand = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    if (!ignore.includes(rand)) {
+        return `https://cdn.jsdelivr.net/gh/${user}/${repo}@main/videos/${rand}_${resolution}.mp4`;
+    } else {
+        return getRandomVideo();
+    }
+}
+
 function tryToLoad() {
     if (navigator.onLine) {
         document.querySelector("#loading").classList.add("d-none");
         document.querySelector("#offline").classList.add("d-none");
         document.querySelector("#main").classList.remove("d-none");
 
-        var videoDiv = document.createElement('div');
-        videoDiv.innerHTML = '<video id="background-video" autoplay loop muted src="https://cdn.jsdelivr.net/gh/bartekl1/MeteoHomepage@main/videos/2.mp4"></video>';
-        document.querySelector('body').appendChild(videoDiv)
+        var videoDiv = document.createElement("div");
+        videoDiv.innerHTML = `<video id="background-video" autoplay loop muted src="${getRandomVideo()}"></video>`;
+        document.querySelector("body").appendChild(videoDiv);
     } else {
         document.querySelector("#loading").classList.add("d-none");
         document.querySelector("#offline").classList.remove("d-none");
